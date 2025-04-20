d6727e0564b531824d2e7fcaffaf753d8e2fd632
Szymon Wieczorek <shateq21@gmail.com>
2025-04-20 10:29:37 +0200

    update scripts

--- config.h
+++ config.h
@@ -1,11 +1,12 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
-	/*Icon*/ /*Command*/		/*Interval*/	/*Signal*/
-	{"", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g", 60, 0},
-	{"", "sb-spotify",      0,    11},
-	{"", "bar-weather 1",	1800, 2},
-	{"", "date '+%I:%M%p'",	60,   1},
+/*Icon*/  /*Command*/       /*Interval*/    /*Signal*/
+	{"", "bar-memory",              180,        3},
+	{"", "bar-player",              0,          6},
+	{"", "bar-volume",  	        0,          5},
+	{"", "bar-weather 1",	        1800,       2},
+	{"", "date +'%d %b %y, %H:%M'",	60,         1},
 };
 
 //sets delimiter between status commands. NULL character ('\0') means no delimiter.
-static char *delim = " :: ";
+static char *delim = " ~~ ";
